a0c4b4ef09d28dcee94f2ebba17c8781c75075cd
Alan Wen <36051502+wenjalan@users.noreply.github.com>
2021-01-24 00:06:38 -0800

    block sentinel change
    changed sentinels to <|start-block|> and <|end-block|> for easier GPT-2 compatibility

--- src/main/java/wenjalan/starbot/gpt/StarbotGPT.java
+++ src/main/java/wenjalan/starbot/gpt/StarbotGPT.java
@@ -50,15 +50,18 @@ public class StarbotGPT {
         // get a list of messages representing the entire history of the channel
         System.out.println("Loading messages...");
         List<Message> history = new LinkedList<>();
-        channel.getIterableHistory().forEach(msg -> history.add(0, msg));
+        channel.getIterableHistory().forEach(msg -> {
+            history.add(0, msg);
+            if (history.size() % 1000 == 0) {
+                System.out.println("Loaded " + history.size() + " messages so far...");
+            }
+        });
         System.out.println("Finished loading " + history.size() + " messages, beginning annotation...");
 
         // list of messages
         List<String> messages = new ArrayList<>();
         // total seen
         AtomicLong seen = new AtomicLong();
-        // last author
-        final AtomicLong lastAuthorId = new AtomicLong();
         // if we're in a block
         AtomicBoolean inBlock = new AtomicBoolean(false);
         // the OffsetDateTime of the last message
@@ -68,7 +71,7 @@ public class StarbotGPT {
         history.forEach((msg) -> {
             // print our progress
             if (seen.get() % 1000 == 0) {
-                System.out.println("Read " + messages.size() + " messages so far...");
+                System.out.println("Annotated " + messages.size() + " messages so far...");
             }
 
             // run content checks
@@ -76,34 +79,34 @@ public class StarbotGPT {
                 // get message details
                 String content = msg.getContentRaw();
 
-                // if this message was sent by the same author as the last one, and this message was sent
-                if (lastMsg.get() != null && isInBlock(lastMsg.get(), msg)) {
-                    // if this is the start of a block
+                // if this message was written by the same author as the last one,
+                // and we haven't set the start block tag
+                if (isInBlock(lastMsg.get(), msg)) {
+                    // if we're not in a block yet, declare one
                     if (!inBlock.get()) {
-                        // set inBlock to true
+                        // set inBlock to true and add a starting tag to the previous message
                         inBlock.set(true);
-
-                        // get the last message and add an opening block
                         String lastMessage = messages.remove(messages.size() - 1);
-                        lastMessage = "<block>" + lastMessage;
+                        lastMessage = "<|start-block|>" + lastMessage;
                         messages.add(lastMessage);
                     }
-                    // otherwise, continue normally
                 }
-                // if it wasn't, check if we were in a block
-                else if (inBlock.get()) {
-                    // in which case append a </block> to the last message
+                // if the authors are different (and this isn't the very first message)
+                else if (lastMsg.get() != null) {
+                    // add an ending tag to the previous message
                     String lastMessage = messages.remove(messages.size() - 1);
-                    lastMessage += "</block>";
+                    lastMessage = lastMessage + "<|end-block|>";
+
+                    // if we weren't in a block previously
+                    // declare the previous message the start
+                    if (!inBlock.get()) {
+                        lastMessage = "<|start-block|>" + lastMessage;
+                    }
                     messages.add(lastMessage);
+
+                    // declare that we're no longer in a block
                     inBlock.set(false);
                 }
-
-                // append message sentinels
-                content = "<msg>" + content + "</msg>";
-
-                // set last author
-                lastAuthorId.set(msg.getAuthor().getIdLong());
 
                 // set last date time
                 lastMsg.set(msg);
@@ -116,13 +119,15 @@ public class StarbotGPT {
             seen.getAndIncrement();
         });
 
-        // if we were in a block when we completed, finish the block
-        if (inBlock.get()) {
-            // in which case append a </block> to the last message
-            String lastMessage = messages.remove(messages.size() - 1);
-            lastMessage += "</block>";
-            messages.add(lastMessage);
+        // finish the last msg
+        String lastMessage = messages.remove(messages.size() - 1);
+        // if we weren't in a block make this the start too
+        if (!inBlock.get()) {
+            lastMessage = "<|start-block|>" + lastMessage;
         }
+        // declare this message the end of the final block
+        lastMessage += "<|end-block|>";
+        messages.add(lastMessage);
 
         // sout
         System.out.println("Download complete, found " + messages.size() + " valid messages");
@@ -148,6 +153,9 @@ public class StarbotGPT {
 
     // returns whether two messages are in a block together
     private static boolean isInBlock(Message lastMsg, Message msg) {
+        // lastMsg is null
+        if (lastMsg == null) return false;
+
         // same author
         if (lastMsg.getAuthor().getIdLong() != msg.getAuthor().getIdLong()) return false;
 
@@ -159,7 +167,7 @@ public class StarbotGPT {
     // returns whether this message is valid for modeling
     private static boolean isValid(Message msg) {
         // get content
-        String content = msg.getContentRaw();
+        String content = msg.getContentRaw().trim();
 
         // if it's sent by a bot
         if (msg.getAuthor().isBot()) return false;
